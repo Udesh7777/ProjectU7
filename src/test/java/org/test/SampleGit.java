aa01d118be063b6c660afaea7ab5007209d66046
Udesh7777 <udesh7777@gmail.com>
2021-01-09 13:31:09 +0530

    second push

--- src/test/java/org/test/SampleGit.java
+++ src/test/java/org/test/SampleGit.java
@@ -1,10 +1,12 @@
 package org.test;
 
+import org.apache.poi.util.SystemOutLogger;
+
 public class SampleGit {
 	
 	public static void main(String[] args) {
 		System.out.println("Program");
-		
+		System.out.println("java");
 	}
 
 }
